02464ecfce01c3ac1d782ca97d2a7f1f9b3e7b22
RohanSabnis <rohansabnis@gmail.com>
2018-06-25 02:21:42 -0700

    Delete errno.h

--- include/errno.h
+++
@@ -1,9 +0,0 @@
-
-
-#define OK              0  /* No error                                 */
-#define ERR             1  /* Not owner                                */
-#define ENOENT           2  /* No such file or directory                */
-#define ESRCH            3  /* No such process                          */
-#define EINTR            4  /* Interrupted system call                  */
-#define EIO              5  /* I/O error                                */
-#define ENXIO            6  /* No such device or address                */
