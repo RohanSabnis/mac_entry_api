bfdba79f43f1fa5f03b9096b62b5ec37f13ffa7b
rsabnis <rsabnis@cisco.com>
2018-06-25 02:12:21 -0700

    API impl

---
+++ include/tables.h
@@ -0,0 +1,88 @@
+/*
+ * --------------------------------------------
+ * tables.h
+ *
+ * Contains all the various structures used for the mac-entry API utility
+ */
+
+#ifndef __TABLES_H__
+#define __TABLES_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "avltree.h"
+
+typedef unsigned char uchar;
+typedef avltree_t mac_tree;
+typedef avltree_t mac_entry_avl_t;
+typedef avltree_t topo_avl_node_t;
+typedef uint32_t topo_id_t;
+
+#define MAC_ADDR_BYTES 6
+#define MAC_ADDR_STRING 19
+#define TOPO_NAME_STRING_MAX_LEN 15
+#define PORT_STRING_MAX_LEN 10
+#define OK 0  /* No error */
+#define ERR 1  /* er */
+#define AGEOUT_VALUE 10
+
+typedef enum {
+	EOK = 0,
+	EERR,
+} err_types_t;
+
+/**
+ * MAC Entry type
+ */
+typedef enum {
+    MAC_ENTRY_TYPE_INVALID = 0,
+    MAC_ENTRY_TYPE_DYNAMIC,
+    MAC_ENTRY_TYPE_STATIC,
+} mac_entry_type_t;
+
+/**
+ * Topology entry
+ */
+typedef struct {
+    /** Key: Topology id */
+    topo_id_t topo_id;
+    /* Data */
+    char topo_name[TOPO_NAME_STRING_MAX_LEN];
+    /** Per topo MAC entry table ptr */
+    mac_tree *mac_tree;
+} topo_entry_t;
+
+/**
+ * Type definition for the mac address.
+ */
+typedef struct {
+	/** Linkage to topo entry in topo table */
+	topo_entry_t *topo_entry_ptr;
+    /** Key: MAC address data */
+    uchar mac_address[MAC_ADDR_BYTES];
+    /** Entry Type (Dynamic/Static) */
+    mac_entry_type_t type;
+    /** Port where MAC was learnt */
+    char port[PORT_STRING_MAX_LEN];
+    /** timestamp when mac was learnt */
+    time_t learn_timestamp;
+    size_t timer_id;
+} mac_entry_t;
+
+/* Structure of a stack node. Linked List implementation is used for
+   stack. A stack node contains a pointer to tree node and a pointer to
+   next stack node */
+typedef struct sNode
+{
+	avltree_t *t;
+	struct sNode *next;
+} sNode_t;
+
+
+
+
+
+
+#endif /* __TABLES_H__ */
---
+++ test/test.c
@@ -0,0 +1,151 @@
+/*
+ ============================================================================
+ Name        : test.c
+ Description : Test file for the MAC table API
+ ============================================================================
+ */
+#include "util.h"
+
+void add_macs_per_topo(int num_of_macs, int num_of_topos) {
+
+	int i=0, j=0;
+	uchar mac_address[MAC_ADDR_BYTES];
+	char topo_name[TOPO_NAME_STRING_MAX_LEN];
+	char intf_name[6];
+	char mac_addr_str[MAC_ADDR_STRING];
+
+	for (i=1; i<= num_of_topos; i++) {
+		memset(topo_name, 0, sizeof(topo_name));
+		memset(intf_name, 0, sizeof(intf_name));
+		sprintf(topo_name, "topo_%d", i);
+		sprintf(intf_name, "Fa0/%d", i);
+		for (j=1; j<=num_of_macs; j++) {
+			memset(mac_addr_str, 0, sizeof(mac_addr_str));
+			snprintf(mac_addr_str, MAC_ADDR_STRING, "AA:AA:AA:AA:AA:%d%d", i, j);
+			uchar bytes[6];
+			uint8_t values[6];
+			int z =0;
+
+			if( 6 == sscanf( mac_addr_str, "%x:%x:%x:%x:%x:%x%*c",
+			    &values[0], &values[1], &values[2],
+			    &values[3], &values[4], &values[5] ) )
+			{
+			    /* convert to uint8_t */
+			    for( z = 0; z < 6; ++z )
+			        bytes[z] = (char) values[z];
+			}
+
+			//strncpy(mac_address, mac_addr_str, MAC_ADDR_BYTES);
+			add_or_update_mac_entry (i, topo_name, bytes, MAC_ENTRY_TYPE_DYNAMIC, intf_name);
+		}
+	}
+}
+
+void
+update_specific_mac_entries() {
+
+	char *mac_addr_str;
+	uchar mac_address[MAC_ADDR_BYTES];
+
+	sleep(5);
+	mac_addr_str = "AA:AA:AA:AA:AA:01";
+	uchar bytes[6];
+	int values[6];
+	int z =0;
+
+	if( 6 == sscanf( mac_addr_str, "%x:%x:%x:%x:%x:%x%*c",
+		&values[0], &values[1], &values[2],
+		&values[3], &values[4], &values[5] ) )
+	{
+		/* convert to uint8_t */
+		for( z = 0; z < 6; ++z )
+			bytes[z] = (uint8_t) values[z];
+	}
+	add_or_update_mac_entry (1, "topo_1", bytes, MAC_ENTRY_TYPE_DYNAMIC, "Fa0/3");
+
+	mac_addr_str = "AA:AA:AA:AA:AA:31";
+	if( 6 == sscanf( mac_addr_str, "%x:%x:%x:%x:%x:%x%*c",
+			&values[0], &values[1], &values[2],
+			&values[3], &values[4], &values[5] ) )
+	{
+		/* convert to uint8_t */
+		for( z = 0; z < 6; ++z )
+			bytes[z] = (uint8_t) values[z];
+	}
+	add_or_update_mac_entry (3, "topo_3", bytes, MAC_ENTRY_TYPE_DYNAMIC, "Fa0/11");
+}
+
+void
+delete_all_mac_entries_for_topo() {
+
+	delete_mac_entries_for_topo (3);
+}
+
+void
+delete_mac_entry_for_specific_topo() {
+
+	char *mac_addr_str;
+	uchar mac_address[MAC_ADDR_BYTES];
+
+	mac_addr_str = "AA:AA:AA:AA:AA:01";
+	uchar bytes[6];
+	int values[6];
+	int z =0;
+
+	if( 6 == sscanf( mac_addr_str, "%x:%x:%x:%x:%x:%x%*c",
+		&values[0], &values[1], &values[2],
+		&values[3], &values[4], &values[5] ) )
+	{
+		/* convert to uint8_t */
+		for( z = 0; z < 6; ++z )
+			bytes[z] = (uint8_t) values[z];
+	}
+	delete_mac_entry_for_given_topo (values, 1);
+
+}
+
+/*
+ * 1. Add all mac entries and wait all of them to expire
+ * 2. Readd all mac entries and then update 2 mac entries, they should expire later than the rest
+ * 3. Readd all mac entries and delete mac entry specific to topo
+ * 5. Delete all mac entries of specfic topo
+ * 6. Rest of the entries should expire
+ */
+
+int main(void) {
+	puts("!!!Executing the various test cases as mentioned below\n"
+			" * 1. Add all mac entries and wait all of them to expire.\n"
+			" * 2. Readd all mac entries and then update 2 mac entries, they should expire later than the rest.\n"
+			" * 3. a. Readd all mac entries and delete specific macs given mac address and topo id.\n"
+			" *    b. Delete all mac entries of specfic topo.\n"
+			" *    c .Rest of the entries should expire!!!");
+
+    initialize();
+
+    printf("\nStarting Test case 1 - Adding 5 macs each for 5 topos and waiting for them to expire\n");
+    add_macs_per_topo(5, 5);
+	sleep(50);
+
+
+	printf("\nStarting Test case 2 - Adding 4 macs each for 3 topos, updating 2 macs and they would expire later\n");
+	add_macs_per_topo(4, 3);
+	update_specific_mac_entries();
+	sleep(40);
+
+
+	printf("\nStarting Test case 3\n"
+			"a. Adding 3 macs for 3 topos, deleting an mac entry for topo 1\n"
+			"b. followed by delete of all mac entries under topo 3\n"
+			"c. Waiting for the rest of the macs to expire(2 macs from topo 1 and 3 macs from topo 2)\n");
+	add_macs_per_topo(3, 3);
+	delete_mac_entry_for_specific_topo();
+	delete_all_mac_entries_for_topo();
+
+	sleep(40);
+
+
+    finalize();
+
+	return EXIT_SUCCESS;
+}
+
